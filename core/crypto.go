445d857c8ecfd101892691f9ce036591f169b049
deltegui <deltegui@hotmail.com>
2025-05-12 13:21:41 +0200

    random password generator

--- core/crypto.go
+++ core/crypto.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"log"
+	"math/big"
 )
 
 const DefaultTokenBytes int = Size32
@@ -18,4 +19,20 @@ func GenerateToken(numberBytes int) string {
 
 func GenerateTokenDefaultLength() string {
 	return GenerateToken(DefaultTokenBytes)
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+
+func GenerateRandomPassword(length int) string {
+	password := make([]byte, length)
+	charsetLength := big.NewInt(int64(len(charset)))
+	for i := range password {
+		index, err := rand.Int(rand.Reader, charsetLength)
+		if err != nil {
+			log.Panicf("Error generating random password: cannot generate random index: %v", err)
+		}
+		password[i] = charset[index.Int64()]
+	}
+
+	return string(password)
 }
